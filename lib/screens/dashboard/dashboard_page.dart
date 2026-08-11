1e61c0fdb64c47e5869a36f3f05eab7afac8cd7d
proyekwili-prog <proyekwili@gmail.com>
2026-08-10 19:41:29 -0700

    rr4

--- lib/screens/dashboard/dashboard_page.dart
+++ lib/screens/dashboard/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/home_page.dart';
+import 'package:flutter_application_1/screens/profil_page.dart';
 import 'package:flutter_application_1/screens/survey_page.dart';
 
 class DashboardPage extends StatefulWidget {
